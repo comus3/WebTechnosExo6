a61d6de40340a9df46e03b3572ea4c2ab2c8d73a
comus3 <come.plantin.carrenard@gmail.com>
2023-11-24 18:17:11 +0100

    pushing progrss. working user check

--- server.js
+++ server.js
@@ -10,18 +10,52 @@ app.use(express.urlencoded({extended:true}));
 
 let connection = require('./db.js');
 
-const getUserData = (response) => {
-    connection.query("SELECT * FROM usertablecourse;", function (error, resultSQL) {
+const tryUser = (username) => {
+    // SQL query to check if the username exists in the 'name' table and get the row number
+    const sqlQuery = 'SELECT ROW_NUMBER() OVER (ORDER BY id) AS row_num FROM name WHERE username = ?';
+
+    // Execute the query
+    connection.query(sqlQuery, [username], (error, results) => {
         if (error) {
-        response.status(400).send(error);
-        } else {
-        let dataBase = {}
-        response.status(200);
-        dataBase = resultSQL;
-        console.log(dataBase);
-        response.render('home.ejs', { taskTable: dataBase });
+            console.error('Error executing SQL query:', error);
+            return;
         }
+
+        if (results.length > 0) {
+            const rowIndex = results[0].row_num;
+            return rowIndex;
+        } else {
+            console.log('Username not found in the name table.');
+            return 0;
+        }
+
     });
+}
+
+const getUserData = (response,userID) => {
+    const sqlQuery = `SELECT * FROM your_table_name WHERE first_column_name = ${indexToQuery}`;
+
+    // Execute the query for relation table
+    connection.query(sqlQuery, (error, results, fields) => {
+    if (error) {
+        console.error('Error executing query for relation table:', error);
+    } else {
+        courseIDs = results;
+        connection.query("SELECT * FROM usertablecourse;", function (error, resultSQL) {
+            if (error) {
+            response.status(400).send(error);
+            } else {
+            let dataBase = {}
+            response.status(200);
+            dataBase = resultSQL;
+            console.log(dataBase);
+            response.render('consultations.ejs', { dataList: dataBase });
+            }
+        });
+    }
+    });
+
+
 };
 const getCourseData = (response) => {
     connection.query("SELECT * FROM tablecourse;", function (error, resultSQL) {
@@ -32,12 +66,28 @@ const getCourseData = (response) => {
         response.status(200);
         dataBase = resultSQL;
         console.log(dataBase);
-        response.render('home.ejs', { taskTable: dataBase });
+        response.render('home.ejs', { dataList: dataBase });
         }
     });
 };
 
+app.post('/login', (request,response)=>{
 
+    if (request.body.username != null){
+        let userID = tryUser(request.body.username);
+        if (userID != 0){
+            getUserData(response,userID);
+        }
+        else{getCourseData(response)};
+    }
+    else{
+        getCourseData(response)
+    }
+})
+
+app.get('/',(request,response)=>{
+    getCourseData(response, connection);
+})
 
 app.use(express.static('public'));
 
